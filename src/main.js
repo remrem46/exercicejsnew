042a8a142b6ca248c2059afdf73a91b2c5f5e3fd
remrem46 <remi.galbois@hotmail.fr>
2025-09-25 10:17:26 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import './style.css'
 import {apiKey} from './env.js'
-// const meteo = fetch("https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=609ce76f324f69c7cf2a301bc08fb18c")
-// .then(response=>{
-//   console.log(response);
-//   return response;
-// })
-// .then((data)=>{
-//   console.log(data.json());
-// });
+const meteo = fetch("https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=609ce76f324f69c7cf2a301bc08fb18c")
+.then(response=>{
+  console.log(response);
+  return response;
+})
+.then((data)=>{
+  console.log(data.json());
+});
 
-// console.log(meteo);
+console.log(meteo);
 
 const apiMeteo = async () =>{//deuxiemme façon de traiter une requete avec sa reponse
   return await fetch('https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=' + apiKey)
